59fc78eeaf46593ad38ccd8da376b86582d394ba
Aporter53 <pavelow53.andrew@gmail.com>
2017-04-11 14:00:00 -0400

    Changed download_url in setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,6 @@ setup(
   author_email='porter.r.andrew@gmail.com',
   license=_license,
   url='https://github.com/AndrewRPorter/nasdaq-stock-quote',
-  download_url='https://github.com/AndrewRPorter/nasdaq-stock-quote/archive/1.1.0.tar.gz',
+  download_url='https://github.com/AndrewRPorter/nasdaq-stock-quote/archive/1.2.0.tar.gz',
   install_requires=['setuptools', 'requests', 'lxml'],
  )
